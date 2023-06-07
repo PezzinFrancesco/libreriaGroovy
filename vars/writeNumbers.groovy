b0f83fb8332f52becc4360e0deaa0be0c32a22ba
EzioGreggioOfficial <123938125+PezzinFrancesco@users.noreply.github.com>
2023-06-07 09:19:25 +0200

    Update writeNumbers.groovy

--- vars/writeNumbers.groovy
+++ vars/writeNumbers.groovy
@@ -1,5 +1,6 @@
-def writeNums(String num){
-  String num2 = (num.toInteger() + 1).toString;
-  String ris = num + " " + num2;
-  return ris;
+def writeNums(int num){
+  /*String num2 = (num.toInteger() + 1).toString;
+  String ris = num + " " + num2;*/
+  
+  return num++;
 }
